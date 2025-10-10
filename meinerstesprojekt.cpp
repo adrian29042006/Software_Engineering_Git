15e03cf6420b5eec56936bc4bf4f59b54cadd9fe
adrian29042006 <ad.haering06@gmail.com>
2025-10-10 17:03:17 +0200

    bruder ich trainiere grad

---
+++ meinerstesprojekt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int hi(){
+    cout<< "HI" << endl;
+    return 0;
+
+}
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    cout << "DIGGA DU SPASSSSSSSSSSTI" << endl;
+    cout << "HI" << endl;
+        cout << "HI" << endl;
+    hi();
+    hi();
+    
+    return 0;
+}
