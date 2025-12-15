d9838031eead4fb8cd6c7de67855d498a28f6717
adrian29042006 <ad.haering06@gmail.com>
2025-12-15 10:14:07 +0100

    Create TemperaturSensor.hpp

---
+++ src/TemperaturSensor.hpp
@@ -0,0 +1,10 @@
+// TemperatureSensor.hpp
+#ifndef TEMPERATURE_SENSOR_HPP
+#define TEMPERATURE_SENSOR_HPP
+
+class TemperatureSensor {
+public:
+    float readCelsius();   // R3.1: Temperatur wird überwacht
+};
+
+#endif // TEMPERATURE_SENSOR_HPP
